88bc3821f32b211cff09989282a33ff45e874e08
Sandmountain <sandtain@gmail.com>
2019-02-20 11:05:36 +0100

    Retry

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
-d3.csv("./Database/scrubbed.csv").then(function(data) {
+awesomeParse = d3.csv("./Database/scrubbed.csv", function(data) {
+  return {
+    datatime: data.Datetime, // convert "Year" column to Date
+   	city: data.city,
+   	country: data.country,
+   	shape: data.shape,
+   	duration: +data.durationSeconds,
+   	comments: data.comments,
+   	latitude: +data.latitude,
+   	longitude: +data.longitude,
+  };	
+}).then(function(data){
     console.log(data);
 });
