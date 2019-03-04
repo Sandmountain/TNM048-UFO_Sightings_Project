a8ff81ab5a253d28bef6bbdbd79f65d13540238f
Sandmountain <sandtain@gmail.com>
2019-03-04 09:38:00 +0100

    Update main.js
    Added new variables from database

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,22 @@ var worldMap, focus_plus_context, points;
 d3.csv("./Database/real_estate_db.csv", function(data) {
   return {
    	latitude: +data.lat,
-   	longitude: +data.lng 
+   	longitude: +data.lng,
+   	state: data.state,
+   	city: data.city,
+   	pop: +data.pop,
+   	male_pop: +data.male_pop,
+   	female_pop: +data.female_pop,
+   	dept: +data.dept,
+   	hs_degree: +data.hs_degree,
+	hs_degree_male: +data.hs_degree_male,
+	hs_degree_female: +data.hs_degree_female,
+	rent_mean: +data.rent_mean,
+	hc_mortgage_mean: +data.hc_mortgage_mean,
+	hc_mortgage_stdev: +data.hc_mortgage_stdev,
+	hi_mean: +data.hi_mean,
+	hi_samples: +data.hi_samples,
+	hi_stdev: +data.hi_stdev,
   };	
 }).then(function(data){
     //points = new Points();
